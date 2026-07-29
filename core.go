c37ac2dd6a6477e208b270cffbf4e11e715fe4f9
CAVEMAN <62531963+cavemanlee@users.noreply.github.com>
2026-07-29 18:47:46 +0800

    Release PSD Optimizer 1.7.6 (#3)
    Co-authored-by: CAVEMAN <cavemanlee@users.noreply.github.com>

--- core.go
+++ core.go
@@ -18,7 +18,7 @@ import (
 const (
 	pluginID         = "caveman.optimizer.uxp"
 	pluginName       = "PSD Optimizer"
-	pluginVersion    = "1.7.5"
+	pluginVersion    = "1.7.6"
 	hostMinVersion   = "25.0.0"
 	registryPathHint = `$localPlugins\External\` + pluginID
 )
